cb51f0848f89301c01eb15e283a067fd75c79a2f
Storozheva Nataliia <storozheva@i.ua>
2020-05-26 10:52:00 +0300

    Initial Commit

---
+++ HW_22_table_comments/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HW_22_table_comments
+//
+//  Created by Nataliia Storozheva on 26.05.2020.
+//  Copyright © 2020 Nataliia Storozheva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
